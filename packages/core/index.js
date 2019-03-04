2f01db44463210292cd420c9312eee49d36b79ca
sunhaolin <sunhaolin@hotoa.com>
2019-03-04 10:42:44 +0800

    revise

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,1 +1,1 @@
-module.exports = require('lib/loadObjectToDB');
+module.exports = require('./lib/loadObjectToDB');
--- packages/core/lib/loadObjectToDB.js
+++ packages/core/lib/loadObjectToDB.js
@@ -1,12 +1,13 @@
 exports.loadObjectToDB = function (obj) {
-    var collection = Cretor.getCollection('Objects');
+    var collection = Creator.getCollection('objects');
+    // TODO fix: Meteor code must always run within a Fiber.
     collection.upsert({
         name: obj.name
     }, obj);
 }
 
 exports.loadTriggerToDB = function (trigger) {
-    var collection = Cretor.getCollection('Objects');
+    var collection = Creator.getCollection('objects');
     collection.upsert({
         name: trigger.name
     }, trigger);
