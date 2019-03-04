961a71ce378a97acdd652712f3accbc4375fef1d
Jack Zhuang <steedos@users.noreply.github.com>
2019-03-04 11:41:31 +0800

    在 settings 中配置环境变量

--- packages/meteor-bundle-runner/index.js
+++ packages/meteor-bundle-runner/index.js
@@ -1,29 +1,67 @@
 var fs = require("fs")
 var os = require("os")
 var path = require("path")
+var _ = require("underscore")
+var yaml = require("js-yaml")
 
-var serverDir = process.env.SERVER_DIR;  
-if (!serverDir) {
-    process.env.NODE_OPTIONS="";
-    var npmRoot = require('child_process').execSync('npm root -g').toString().trim()
-    try {
-        serverDir = path.join(npmRoot, "steedos-server", "bundle", "programs", "server")
-    } catch (err) {
-        console.error(`Install steedos server globally first with: npm install -g steedos-server`)
-        process.exit(1)
-    }
-} else {
-    serverDir = path.join(serverDir, "bundle", "programs", "server");
+
+var getServerDir = function(){
+  var serverDir = process.env.SERVER_DIR;  
+  if (!serverDir) {
+      process.env.NODE_OPTIONS="";
+      var npmRoot = require('child_process').execSync('npm root -g').toString().trim()
+      try {
+          serverDir = path.join(npmRoot, "steedos-server", "bundle", "programs", "server")
+      } catch (err) {
+          console.error(`Install steedos server globally first with: npm install -g steedos-server`)
+          process.exit(1)
+      }
+  } else {
+      serverDir = path.join(serverDir, "bundle", "programs", "server");
+  }
+  
+  if (!fs.existsSync(serverDir))
+  {
+      console.error(`serverDir not found: ${serverDir}`);
+      console.error(`Install steedos server globally first with: npm install -g steedos-server`)
+      process.exit(1)
+  }  
+  return serverDir;
 }
 
-if (!fs.existsSync(serverDir))
-{
-    console.error(`serverDir not found: ${serverDir}`);
-    console.error(`Install steedos server globally first with: npm install -g steedos-server`)
-    process.exit(1)
-}  
+var getSettings = function(){
+  
+  var settingsFileName = 'settings.yml'
+  var settingsPath = path.join(projectDir, settingsFileName)
+  var settings = {
+  };
+  if(fs.existsSync(settingsPath)){
+    try {
+      settings = yaml.safeLoad(fs.readFileSync(settingsPath, 'utf8'));
+    } catch (error) {
+      console.error(`Invalid settings.yml`, error)
+    }
+  }
+
+  if(settings){
+    process.env.METEOR_SETTINGS = JSON.stringify(settings)
+  }
+
+  // 环境变量，以settings中定义的为准
+  if (settings.env){
+    _.each(settings.env, function(item, key){
+      process.env[key] = item
+    })
+  }
+
+  return settings;
+
+}
   
 var projectDir = process.cwd();   
+var serverDir = getServerDir();
+var settings = getSettings();
+
 
 var port = process.env.PORT;
 if (!port){
@@ -41,20 +79,6 @@ var mongoUrl =  process.env.MONGO_URL;
 if (!mongoUrl) {  
   mongoUrl = "mongodb://127.0.0.1/steedos";
   process.env.MONGO_URL = mongoUrl
-}
-
-var settingsFileName = 'settings.yml'
-var settingsPath = path.join(projectDir, settingsFileName)
-var settings;
-if(fs.existsSync(settingsPath)){
-  try {
-    settings = yaml.safeLoad(fs.readFileSync(settingsPath, 'utf8'));
-  } catch (error) {
-    this.log(`Invalid settings.yml`, error)
-  }
-}
-if(settings){
-  process.env.METEOR_SETTINGS = JSON.stringify(settings)
 }
 
 __steedos_bootstrap__ = {
